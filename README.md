3bd052d3f0ba2ed6c5f0777f1f0af087b7bdcd28
Aditya <adi.addepalli@gmail.com>
2022-09-25 17:42:15 -0500

    Some updates

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## About
 This is a simple repository with the code for a bot I made for a close friend and amazing streamer, [Vifer](https://www.twitch.tv/vifer).
 
-You can use the '$random' command when the bot is linked to your server to send a randomized part of an Arknights character for you to guess.
+You can use the `$random` command when the bot is linked to your server to send a randomized part of an Arknights character for you to guess.
 
 ## Technologies used
 - Python
@@ -11,4 +11,6 @@ You can use the '$random' command when the bot is linked to your server to send 
 - Git
 - Discord API
 
-You can find the meat & potatoes of the code [here](src/main.py). Please join Vifer's discord and give him a follow on his socials!
+You can find the meat & potatoes of the code [here](src/main.py). 
+
+Please join Vifer's discord and give him a follow on his socials!
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from array import array
+from ast import Bytes
 import discord
 import os
 from discord.ext import commands
@@ -7,6 +8,13 @@ from dotenv import load_dotenv
 import dataa
 from arrayData import arrayImages
 #import tensorflow as tf
+from PIL import Image
+from random import randrange
+import requests
+import io
+import torch
+import torchvision.transforms as T
+import base64
 
 load_dotenv() 
 
@@ -26,20 +34,26 @@ def validate_image(imageName):
     if imageName.lower() == the_key:
       return True
       break
+
 #TODO: randomly crop a part of the image
-# def random_crop(image):
-#  cropped_image = tf.image.random_crop(
-#      image, size=[100, 100, 3])
+def random_crop(image):
+  
+  left = 0
+  top = 50
+  right = 510
+  bottom = 292
 
-#  return cropped_image
+  img_res = image.crop((left, top, right, bottom)) 
+  #cropped_image = tf.image.random_crop(image, size=[100, 100, 3])
+  return img_res
 
+#TODO: Check if the value entered is the same as the name of the image given
 #def crossCheck(val):
   #if val == 
 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
-  print(arrayImages[1])
 
   
 #@bot.command()
@@ -58,8 +72,44 @@ async def on_message(message):
     #embed.set_image(url=random.choice(list(dataa.dictImages.items())))
     #embed.set_image(url=random.choice(arrayImages[1]))
 
+    # byteImgIO = io.BytesIO()
+    # byteImg = Image.open("some/location/to/a/file/in/my/directories.png")
+    # byteImg.save(byteImgIO, "PNG")
+    # byteImgIO.seek(0)
+    # byteImg = byteImgIO.read()
+
     embed = discord.Embed(title="Guess which character this is:")
-    embed.set_image(url=random.choice(arrayImages))
+
+    def pillow_image_to_base64_string(img):
+      buffered = io.BytesIO()
+      img.save(buffered, format="JPEG")
+      return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+
+    def base64_string_to_pillow_image(base64_str):
+        return Image.open(io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8"))))
+
+
+    # Example for Converting pillow image to base64 data URL to view in browser
+    
+    # You can put this data URL in the address bar of your browser to view the image
+
+    url=random.choice(arrayImages)
+    print("url: ", url)
+    response = requests.get(url)
+    img = Image.open(io.BytesIO(response.content)).convert('RGB')
+    print("image format: ",img.format)
+
+    transform = T.RandomCrop((300,300))
+    img = transform(img)
+    print("show image")
+    print("image cropped format", img.format)
+    img.show()
+    print(img)
+    print("Image mode: ", img.mode)
+
+    # Send image to embed and send embed to response
+    embed.set_image(url=img)
     await message.channel.send(embed=embed)
 
 
